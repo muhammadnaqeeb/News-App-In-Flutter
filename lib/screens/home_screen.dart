7a71edd5e0b6a7ac68315a42c4d2cbc9916a9479
mnaqeeb06 <muhammadnaqeebawan@gmail.com>
2022-08-22 03:27:19 -0700

    Adding Bottom Navbar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,12 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
+import '../widgets/bottom_nav_bar.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   static const routeName = '/';
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      bottomNavigationBar: const BottomNavBar(index: 0),
+      body: Container(),
+    );
   }
 }
