5b694f8861600915157e39e34c571d1b76576014
mnaqeeb06 <muhammadnaqeebawan@gmail.com>
2022-08-22 06:00:07 -0700

    HomeScreen: New Of the Day Section

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
-
+import 'package:news/models/articals_model.dart';
+import 'package:news/widgets/custom_tag.dart';
 import '../widgets/bottom_nav_bar.dart';
+import '../widgets/image_container.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,9 +11,93 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.menu,
+              color: Colors.white,
+            )),
+      ),
+
+      // making container behind the appBar
+      extendBodyBehindAppBar: true,
       bottomNavigationBar: const BottomNavBar(index: 0),
-      body: Container(),
+      body: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          _NewsOfTheDay(),
+        ],
+      ),
+    );
+  }
+}
+
+class _NewsOfTheDay extends StatelessWidget {
+  const _NewsOfTheDay({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ImageContainer(
+      height: MediaQuery.of(context).size.height * 0.45,
+      width: double.infinity,
+      imageUrl: Article.articles[1].imageUrl,
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomTag(
+            backgroundColor: Colors.grey.withAlpha(150),
+            children: [
+              Text(
+                'News of the day',
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Colors.white,
+                    ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10.0,
+          ),
+          Text(
+            Article.articles[0].title,
+            style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                  fontWeight: FontWeight.bold,
+                  height: 1.25,
+                  color: Colors.white,
+                ),
+          ),
+          TextButton(
+            onPressed: () {},
+            style: TextButton.styleFrom(padding: EdgeInsets.zero),
+            child: Row(
+              children: [
+                Text(
+                  'Learn More',
+                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                        fontWeight: FontWeight.bold,
+                        height: 1.25,
+                        color: Colors.white,
+                      ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                const Icon(
+                  Icons.arrow_right_alt,
+                  color: Colors.white,
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
