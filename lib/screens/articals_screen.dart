d2c9dac9b8921c5fc4a49ef6a86974a51ce2556f
mnaqeeb06 <muhammadnaqeebawan@gmail.com>
2022-08-22 22:38:16 -0700

    Adding Articles Screen

--- lib/screens/articals_screen.dart
+++ lib/screens/articals_screen.dart
@@ -1,13 +1,209 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:news/widgets/image_container.dart';
+
+import '../models/articals_model.dart';
+import '../widgets/custom_tag.dart';
 
 class ArticalsScreen extends StatelessWidget {
   const ArticalsScreen({Key? key}) : super(key: key);
 
   static const routeName = '/article';
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final article = ModalRoute.of(context)!.settings.arguments as Article;
+    return ImageContainer(
+      width: double.infinity,
+      imageUrl: article.imageUrl,
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          iconTheme: const IconThemeData(color: Colors.white),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        extendBodyBehindAppBar: true,
+        body: ListView(
+          children: [
+            _NewsHeadline(
+              article: article,
+            ),
+            Container(
+              padding: const EdgeInsets.all(20.0),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20)),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      CustomTag(
+                        backgroundColor: Colors.black,
+                        children: [
+                          CircleAvatar(
+                            radius: 10,
+                            backgroundImage:
+                                NetworkImage(article.authorImageUrl),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            article.author,
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyMedium!
+                                .copyWith(
+                                  color: Colors.white,
+                                ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      CustomTag(
+                        backgroundColor: Colors.grey.shade200,
+                        children: [
+                          const Icon(
+                            Icons.timer,
+                            color: Colors.grey,
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            '${DateTime.now().hour} h',
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      CustomTag(
+                        backgroundColor: Colors.grey.shade200,
+                        children: [
+                          const Icon(
+                            Icons.remove_red_eye,
+                            color: Colors.grey,
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            '${article.views}',
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    article.title,
+                    style: Theme.of(context)
+                        .textTheme
+                        .headlineSmall!
+                        .copyWith(fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    article.body,
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyMedium!
+                        .copyWith(height: 1.5),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  GridView.builder(
+                    shrinkWrap: true,
+                    itemCount: 2,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 1.25,
+                    ),
+                    itemBuilder: ((context, index) {
+                      return ImageContainer(
+                        width: MediaQuery.of(context).size.width * 0.42,
+                        imageUrl: article.imageUrl,
+                        margin: const EdgeInsets.only(right: 5.0, bottom: 5.0),
+                      );
+                    }),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _NewsHeadline extends StatelessWidget {
+  final Article article;
+  const _NewsHeadline({
+    Key? key,
+    required this.article,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.12,
+          ),
+          CustomTag(
+            backgroundColor: Colors.grey.withAlpha(150),
+            children: [
+              Text(
+                article.category,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Colors.white,
+                    ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Text(
+            article.title,
+            style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  height: 1.25,
+                ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Text(
+            article.subtitle,
+            style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                  color: Colors.white,
+                ),
+          ),
+        ],
+      ),
+    );
   }
 }
